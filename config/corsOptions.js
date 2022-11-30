1f084943e138656bd5b17acb3660bf276cec38ef
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-30 16:33:53 +0530

    configured cors to allow requests from front-end on route GET /notes

---
+++ config/corsOptions.js
@@ -0,0 +1,14 @@
+const allowedUrls = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedUrls.indexOf(origin) !== -1) {
+      callback(null, true);
+    }
+    else {
+      callback(new Error('Not allowed ny cors'));
+    }
+  }
+};
+
+module.exports = corsOptions;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
-app.get('/notes', (req, res) => {
+app.get('/notes', cors(corsOptions), (req, res) => {
   const notes = [
     {
       text: "Do homework",
