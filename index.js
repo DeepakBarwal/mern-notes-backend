506720bccbe3f051bbc35b436aeca647d5861ba1
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-30 16:24:00 +0530

    added GET /notes route to send notes to client

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/notes', (req, res) => {
+  const notes = [
+    {
+      text: "Do homework",
+      link: "https://google.com"
+    },
+    {
+      text: "Order food",
+      link: "https://zomato.com"
+    }
+  ];
+  res.json({ notes });
+});
+
 app.listen(PORT, () => {
   console.log(`MERN Notes Backend App running on http://localhost:${PORT}`)
 });
