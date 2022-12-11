8bcef0357a8394fb1aae682520676f5afcb59edd
DeepakBarwal <DeepakBarwal98@gmail.com>
2022-12-11 16:51:02 +0530

    using Note model in /notes GET route to fettch all notes from DB

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const notesRouter = express.Router();
+const mongoose = require('mongoose');
+const Note = require('../../db/models/note.model');
 
-notesRouter.get('/', (req, res) => {
-    const notesData = [
-        {
-          text: "Do homework",
-          link: "https://google.com"
-        },
-        {
-          text: "Order food",
-          link: "https://zomato.com"
-        }
-      ];
-      res.json({ notes: notesData });
+notesRouter.get('/', async (req, res) => {
+    // const notesData = [
+    //     {
+    //       text: "Do homework",
+    //       link: "https://google.com"
+    //     },
+    //     {
+    //       text: "Order food",
+    //       link: "https://zomato.com"
+    //     }
+    //   ];
+    //   res.json({ notes: notesData });
+    const notes = await Note.find({});
+    console.log(notes);
+    res.json({notes});
 });
 
 module.exports = {
